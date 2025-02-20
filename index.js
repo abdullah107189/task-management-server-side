a70475d65f83a9d8ec943f283c39c71d89bcd1ea
abdullah107189 <abdullah107189@gmail.com>
2025-02-20 15:24:50 +0600

    set new user added

--- index.js
+++ index.js
@@ -1,11 +1,66 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4545;
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fx40ttv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db("task_management");
+    const usersCollection = database.collection("users");
+
+    app.post("/setUser", async (req, res) => {
+      const userInfo = req.body;
+      try {
+        // Check if user already exists
+        const query = {
+          email: userInfo.email,
+        };
+        let user = await usersCollection.findOne(query);
+        if (!user) {
+          const result = await usersCollection.insertOne(userInfo);
+          res.send(result);
+        } else {
+          return res.status(200).send({ message: "User  already exists" });
+        }
+      } catch (error) {
+        console.error("Error saving user data:", error);
+        return res.status(500).send({ error: "Failed to save user data" });
+      }
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
